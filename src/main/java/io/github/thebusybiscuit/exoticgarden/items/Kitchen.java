96780892e28bb9a5379ba18a3fcc40f4b0c9c51c
ybw0014 <i@guizhanss.com>
2021-12-16 09:09:02 +0800

    找回丢失的汉化

--- src/main/java/io/github/thebusybiscuit/exoticgarden/items/Kitchen.java
+++ src/main/java/io/github/thebusybiscuit/exoticgarden/items/Kitchen.java
@@ -35,7 +35,7 @@ public class Kitchen extends MultiBlockMachine {
 
     @ParametersAreNonnullByDefault
     public Kitchen(ExoticGarden plugin, ItemGroup itemGroup) {
-        super(itemGroup, new SlimefunItemStack("KITCHEN", Material.CAULDRON, "&eKitchen", "", "&a&oYou can make a bunch of different yummies here!", "&a&oThe result goes in the Furnace output slot"), new ItemStack[] { new CustomItemStack(Material.BRICK_STAIRS, "&oBrick Stairs (upside down)"), new CustomItemStack(Material.BRICK_STAIRS, "&oBrick Stairs (upside down)"), new ItemStack(Material.BRICKS), new ItemStack(Material.STONE_PRESSURE_PLATE), new ItemStack(Material.IRON_TRAPDOOR), new ItemStack(Material.BOOKSHELF), new ItemStack(Material.FURNACE), new ItemStack(Material.DISPENSER), new ItemStack(Material.CRAFTING_TABLE) }, new ItemStack[0], BlockFace.SELF);
+        super(itemGroup, new SlimefunItemStack("KITCHEN", Material.CAULDRON, "&e厨房", "", "&a&o你可以在此做出各式各样的美味!", "&a&o做好的成品可以在熔炉里获取"), new ItemStack[] { new CustomItemStack(Material.BRICK_STAIRS, "&o砖楼梯 (倒着放)"), new CustomItemStack(Material.BRICK_STAIRS, "&o砖楼梯 (倒着放)"), new ItemStack(Material.BRICKS), new ItemStack(Material.STONE_PRESSURE_PLATE), new ItemStack(Material.IRON_TRAPDOOR), new ItemStack(Material.BOOKSHELF), new ItemStack(Material.FURNACE), new ItemStack(Material.DISPENSER), new ItemStack(Material.CRAFTING_TABLE) }, new ItemStack[0], BlockFace.SELF);
 
         this.plugin = plugin;
     }
